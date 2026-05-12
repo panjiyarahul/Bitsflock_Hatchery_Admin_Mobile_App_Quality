579326255dbdff43f553217051abad12c62c484c
Jeevandhl07 <udsjeevan@gmail.com>
2026-05-12 17:30:37 +0545

    Export screen modules

---
+++ src/screens/index.ts
@@ -0,0 +1,45 @@
+//Auth Screens
+import LoginScreen from './LoginScreen';
+
+//DashBoard Screens
+import HomeScreen from './DashboardScreens/HomeScreen';
+import ProfileScreen from './DashboardScreens/ProfileScreen';
+
+//Active Farm Screens
+import FlockListScreen from './DashboardScreens/ActiveFarmScreens/FlockListScreen';
+import PenListScreen from './DashboardScreens/ActiveFarmScreens/PenListScreen';
+import FlockFarmListScreen from './DashboardScreens/ActiveFarmScreens/FlockFarmListScreen';
+import FlockReportScreen from './DashboardScreens/ActiveFarmScreens/FlockReportScreen';
+
+//Closed Batches Screens
+import ClosedBatchesListScreen from './DashboardScreens/ClosedBatchesScreens/ClosedBatchesListScreen';
+import ClosedBatchesFlocksScreen from './DashboardScreens/ClosedBatchesScreens/ClosedBatchesFlocksScreen';
+import ClosedBatchesReportScreen from './DashboardScreens/ClosedBatchesScreens/ClosedBatchesReportScreen';
+
+//Sales Report Screens
+import SalesReportListScreen from './DashboardScreens/SalesReportScreens/SalesReportListScreen';
+import SalesDetailsScreen from './DashboardScreens/SalesReportScreens/SalesDetailsScreen';
+
+export {
+  //Auth Screen
+  LoginScreen,
+
+  //DashBoard Screens
+  HomeScreen,
+  ProfileScreen,
+
+  //Active Farm Screens
+  FlockListScreen,
+  PenListScreen,
+  FlockFarmListScreen,
+  FlockReportScreen,
+
+  //Closed Batches Screens
+  ClosedBatchesListScreen,
+  ClosedBatchesFlocksScreen,
+  ClosedBatchesReportScreen,
+
+  //Sales Report Screens
+  SalesReportListScreen,
+  SalesDetailsScreen,
+};
